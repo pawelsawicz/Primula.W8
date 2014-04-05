157ebd18c6ef3ee841b83aef1f9fc3a7ce93a2c3
pawelsawicz <pawel@sawicz.eu>
2014-04-05 14:09:47 +0200

    resources, converters folders created, also implement ViewModelBase

---
+++ Primula.W8/ViewModels/ViewModelBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace Primula.W8.ViewModels
+{
+    public abstract class ViewModelBase : Screen 
+    {
+        private readonly INavigationService _navigationService;
+
+        protected ViewModelBase(INavigationService navigationService)
+        {
+            this._navigationService = navigationService;
+        }
+
+        public void GoBack()
+        {
+            _navigationService.GoBack();
+        }
+
+        public bool CanGoBack
+        {
+            get
+            {
+                return _navigationService.CanGoBack;
+            }
+        }
+    }
+}
