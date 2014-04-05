e8f2c77f0194fc4a285eeb6559368fefc73d6b2b
pawelsawicz <pawel@sawicz.eu>
2014-04-05 19:19:10 +0200

    styles of LoginView moved to resource file

--- Primula.W8/ViewModels/LoginViewModel.cs
+++ Primula.W8/ViewModels/LoginViewModel.cs
@@ -5,16 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
 using Primula.W8.ViewModels;
+using Primula.W8.Views;
 
 namespace Primula.W8.ViewModels
 {
     public class LoginViewModel : ViewModelBase 
     {
+        private readonly INavigationService _navigationService;
+
         public LoginViewModel(INavigationService navigationService)
             : base(navigationService)
         {
             _title = "Login Page";
+            _navigationService = navigationService;
         }
+
+        public void TryLogin()
+        {
+            _navigationService.Navigate(typeof(DashboardView));
+        }        
 
         protected override void OnInitialize()
         {
