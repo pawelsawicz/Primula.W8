92f81a6397b5fff981ba77cc6d84af5f690daaf1
pawelsawicz <pawel@sawicz.eu>
2014-04-09 00:31:52 +0200

    CashPayment view and viewmodel have been created

---
+++ Primula.W8/ViewModels/CashPaymentViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primula.W8.ViewModels
+{
+    public class CashPaymentViewModel : ViewModelBase
+    {
+        private readonly INavigationService _navigationService;
+
+        public CashPaymentViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            _navigationService = navigationService;
+            _title = "Cash Payment - Step 3/4";
+        }
+
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+        }
+
+    }
+}
