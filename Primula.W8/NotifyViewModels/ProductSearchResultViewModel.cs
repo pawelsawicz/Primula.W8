dc3900536cebdcc0733dd8a9fc006c96bdcf383e
pawelsawicz <pawel@sawicz.eu>
2014-04-21 00:18:37 +0200

    ProductSearchResultViewModel has been created, this model support search engine

---
+++ Primula.W8/NotifyViewModels/ProductSearchResultViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using Primula.W8.Enums;
+
+namespace Primula.W8.NotifyViewModels
+{
+    public class ProductSearchResultViewModel : PropertyChangedBase
+    {
+        public string Name { get; set; }
+        public string Producer { get; set; }
+        public UnitsEnum Unit { get; set; }
+        public decimal BruttoPrice { get; set; }
+        public CurrencyEnum Currency { get; set; }
+    } 
+}
