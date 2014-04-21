ab9695d3a2d5b58eb75e35e87415740511e8cb97
pawelsawicz <pawel@sawicz.eu>
2014-04-21 22:32:31 +0200

    created new ViewModel that represent product in ordercandidate view

---
+++ Primula.W8/NotifyViewModels/ProductOrderViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Caliburn.Micro;
+using Primula.W8.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primula.W8.NotifyViewModels
+{
+    public class ProductOrderViewModel : PropertyChangedBase
+    {
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public UnitsEnum Unit { get; set; }
+        public decimal BruttoPrice { get; set; }
+    }
+}
