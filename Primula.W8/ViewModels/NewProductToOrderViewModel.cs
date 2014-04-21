ea112c75210184f9a73def6d54fb51fe9f703623
pawelsawicz <pawel@sawicz.eu>
2014-04-21 10:53:32 +0200

    create some logic for this View, like search, add, and clear

--- Primula.W8/ViewModels/NewProductToOrderViewModel.cs
+++ Primula.W8/ViewModels/NewProductToOrderViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using Caliburn.Micro;
+using Primula.W8.Enums;
+using Primula.W8.NotifyViewModels;
+using Primula.W8.Views;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +20,13 @@ namespace Primula.W8.ViewModels
         {
             _navigationService = navigationService;
             _title = "Add new product to order view";
+            ProductSearchResult = new BindableCollection<ProductSearchResultViewModel>();
+        }
+
+        public BindableCollection<ProductSearchResultViewModel> ProductSearchResult
+        {
+            get;
+            private set;
         }
 
         private string _title;
@@ -25,6 +36,54 @@ namespace Primula.W8.ViewModels
             {
                 return _title;
             }
+        }
+
+        private string _searchQuery;
+        public string SearchQuery
+        {
+            get
+            {
+                return _searchQuery;
+            }
+            set
+            {
+                _searchQuery = value;
+                NotifyOfPropertyChange(() => SearchQuery);
+            }
+        }
+
+
+        public void ProductSearch()
+        {
+            
+        }
+
+        public bool CanProductSearch
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(SearchQuery))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+
+        public void AddProductToOrder()
+        {            
+            _navigationService.Navigate(typeof(DashboardView));
+        }
+
+        public void ClearProductSearch()
+        {
+            ProductSearchResult.Clear();
+        }
+
+        protected override void OnInitialize()
+        {
+            
         }
     }
 }
